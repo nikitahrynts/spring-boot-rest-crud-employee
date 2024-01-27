3aa0b01e49dec3069bff4c53922feb534fb6f366
Nikita <hrynts2002@gmail.com>
2024-01-27 12:10:24 +0300

    Added Spring Data Rest

--- src/main/java/by/hryntsaliou/springboot/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/by/hryntsaliou/springboot/cruddemo/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package by.hryntsaliou.springboot.cruddemo.dao;
 
 import by.hryntsaliou.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
