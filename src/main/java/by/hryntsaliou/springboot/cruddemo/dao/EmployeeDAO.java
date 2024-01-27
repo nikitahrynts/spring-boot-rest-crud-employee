b80e3a3dcb90335ec29563ad02deef48872295c0
Nikita <hrynts2002@gmail.com>
2024-01-27 05:26:35 +0300

    Initial commit

---
+++ src/main/java/by/hryntsaliou/springboot/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package by.hryntsaliou.springboot.cruddemo.dao;
+
+import by.hryntsaliou.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    List<Employee> findAll();
+
+    Employee findById(int id);
+
+    Employee save(Employee employee);
+
+    void deleteById(int id);
+}
